6b99370aa9fe4d20e2077a5f5366a85b7f1b496b
Jung Kim <jkim@streamnative.io>
2023-09-20 20:37:59 -0700

    OMG IT WORKs!!!!

--- src/connection/pulsarSocket/utils.ts
+++ src/connection/pulsarSocket/utils.ts
@@ -4,8 +4,8 @@ import { BaseCommand } from '../../../src/proto/PulsarApi'
 export const commandToPayload = (command: BaseCommand): Uint8Array => {
   const marshalledCommand = BaseCommand.encode(command).finish()
   const payload = new Uint8Array(4 + 4 + marshalledCommand.length)
-  payload.set((new Writer()).fixed32(4 + marshalledCommand.length).finish().reverse())
-  payload.set((new Writer()).fixed32(marshalledCommand.length).finish().reverse())
-  payload.set(marshalledCommand)
+  payload.set((new Writer()).fixed32(4 + marshalledCommand.length).finish().reverse(), 0)
+  payload.set((new Writer()).fixed32(marshalledCommand.length).finish().reverse(), 4)
+  payload.set(marshalledCommand, 8)
   return payload
 }
